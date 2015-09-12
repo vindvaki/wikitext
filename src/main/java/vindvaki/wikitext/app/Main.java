6696e86279d85a33409c6a29928437e8cdbd7794
Hordur Freyr Yngvason <hordurf@gmail.com>
2015-09-12 22:49:10 +0200

    parse and emit text off the main thread
    It turns out that we can obtain about a 2x speedup by parsing the
    articles in a separate thread.

--- src/main/java/vindvaki/wikitext/app/Main.java
+++ src/main/java/vindvaki/wikitext/app/Main.java
@@ -12,21 +12,42 @@ import org.xml.sax.SAXException;
 import java.io.File;
 import java.io.IOException;
 
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class Main {
-  static class PlainTextFilter implements IArticleFilter {
+
+  static LinkedBlockingQueue<String> rawWikiTextQueue = new LinkedBlockingQueue<String>();
+  static boolean rawTextParsingDone = false;
+
+  static class RawTextFilter implements IArticleFilter {
     @Override
     public void process(WikiArticle page, Siteinfo siteinfo) throws SAXException {
-       if ( page.isMain() ) {
-        String rawWikiText = page.getText();
-        WikiModel wikiModel = new WikiModel("/${image}", "$/{title}");
-        PlainTextConverter converter = new PlainTextConverter();
-        String plainText = wikiModel.render(converter, rawWikiText);
-        System.out.println(plainText);
+      if ( page.isMain() ) {
+        rawWikiTextQueue.add(page.toString());
       }
     }
   }
-  
+
+  static class PlainTextEmitter implements Runnable {
+    static final PlainTextConverter CONVERTER = new PlainTextConverter();
+    static final WikiModel WIKI_MODEL = new WikiModel("/${image}", "$/{title}");
+    public void run() {
+      while ( !rawTextParsingDone || !rawWikiTextQueue.isEmpty() ) {
+        try {
+          String rawWikiText = rawWikiTextQueue.take();
+          String plainText = WIKI_MODEL.render(CONVERTER, rawWikiText);
+          System.out.println(plainText);
+        } catch (InterruptedException e) {
+          Thread.currentThread().interrupt();
+        }
+      }
+    }
+  }
+
   public static void main(String[] args) throws IOException, SAXException {
-    new WikiXMLParser(new File(args[0]), new PlainTextFilter()).parse();
+    Thread plainTextEmitter = new Thread(new PlainTextEmitter());
+    plainTextEmitter.start();
+    new WikiXMLParser(new File(args[0]), new RawTextFilter()).parse();
+    rawTextParsingDone = true;
   }
 }
